e98d6c4ced7438ab2fac55e5101112feb1a3dfd8
Orlandy Ariel <alu0100773408@ull.edu.es>
2016-11-17 15:56:56 +0000

    inicio proyecto

---
+++ src/main/java/ull/herramientas/OpenDataChartGenerator_ODCG/App.java
@@ -0,0 +1,13 @@
+package ull.herramientas.OpenDataChartGenerator_ODCG;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
