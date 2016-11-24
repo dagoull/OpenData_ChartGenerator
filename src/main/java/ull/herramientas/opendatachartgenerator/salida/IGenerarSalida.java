6e5aeca2119075ecc7988b3b0bfd5cb88c72f03f
Orlandy Ariel Sánchez <alu0100773408@ull.edu.es>
2016-11-24 12:02:09 +0000

    cambios de la salida, ahora devuelve JFreeChart TG-2

--- src/main/java/ull/herramientas/opendatachartgenerator/salida/IGenerarSalida.java
+++ src/main/java/ull/herramientas/opendatachartgenerator/salida/IGenerarSalida.java
@@ -1,8 +1,8 @@
 package ull.herramientas.opendatachartgenerator.salida;
 
-import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
 
 public interface IGenerarSalida
 {
-	public ChartFrame salida();
+	public JFreeChart salida();
 }
