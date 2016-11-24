daebc8e13d5be8812c7af989edb223621ce48d9d
alu0100723304 <alu0100723304@ull.edu.es>
2016-11-24 11:46:23 +0000

    Subiendo ficheros comentados TG-4

--- src/main/java/ull/herramientas/opendatachartgenerator/App.java
+++ src/main/java/ull/herramientas/opendatachartgenerator/App.java
@@ -2,6 +2,22 @@ package ull.herramientas.opendatachartgenerator;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo!");
+		Atributo A = new Atributo("Joel", "Nombre");
+		Atributo B = new Atributo("24", "Edad");
+		Atributo C = new Atributo("Tlf");
+		
+		Instancia I = new Instancia();
+		I.addAtributo(A);
+		I.addAtributo(B);
+		I.addAtributo(C);
+		
+		for (int i = 0; i < I.size(); i++) {
+			System.out.print(I.getIdItem(i) + "\t");
+		}
+		
+		System.out.println();
+		for (int i = 0; i < I.size(); i++) {
+			System.out.print(I.getValorItem(i) + "\t");
+		}
 	}
 }
