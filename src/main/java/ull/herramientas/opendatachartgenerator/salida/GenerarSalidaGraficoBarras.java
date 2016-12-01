8601194ec49203bd45c273cfa6aa60807a5f9acc
Orlandy Ariel Sánchez <alu0100773408@ull.edu.es>
2016-12-01 18:47:03 +0000

    GenerarSalidaGraficoBarras, adaptandolo para leer del Dataset

--- src/main/java/ull/herramientas/opendatachartgenerator/salida/GenerarSalidaGraficoBarras.java
+++ src/main/java/ull/herramientas/opendatachartgenerator/salida/GenerarSalidaGraficoBarras.java
@@ -1,5 +1,6 @@
 package ull.herramientas.opendatachartgenerator.salida;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.jfree.chart.ChartFactory;
@@ -7,6 +8,9 @@ import org.jfree.chart.ChartFrame;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
+
+import ull.herramientas.opendatachartgenerator.Dataset;
+import ull.herramientas.opendatachartgenerator.Instancia;
 
 /**
  * \class SalidaGraficosFrame \brief Clase que genera las salidas en grafico de
@@ -17,6 +21,7 @@ public class GenerarSalidaGraficoBarras implements IGenerarSalida
 { // ATRIBUTOS
 	private DefaultCategoryDataset m_dataset;
 	private List<List<String>> m_lista;
+	private Dataset m_dataset_d;
 	private JFreeChart m_salida;
 
 	// CONSTRUCTOR/ES Y METODOS
@@ -24,6 +29,10 @@ public class GenerarSalidaGraficoBarras implements IGenerarSalida
 	{
 		m_lista = arrayList;
 		configurarDataSet();
+	}
+	public GenerarSalidaGraficoBarras(Dataset a_data)
+	{
+		m_dataset_d= a_data;
 	}
 
 	/**
@@ -41,6 +50,20 @@ public class GenerarSalidaGraficoBarras implements IGenerarSalida
 			for (int j = 0; j < m_lista.get(i).size(); j++)
 			{
 				m_dataset.addValue(j, "ayer", m_lista.get(i).get(j));
+			}
+		}
+	}
+	//cambiar nombre de este método
+	private void configurarDataSetPropio()
+	{
+		m_dataset = new DefaultCategoryDataset();
+		String [] t_array= m_dataset_d.getColumnas();
+		for (int i = 0; i < t_array.length; i++)
+		{
+			ArrayList<Instancia> t_a= m_dataset_d.getRows();
+			for (int j = 0; j <t_a.size() ; j++)
+			{
+				
 			}
 		}
 	}
