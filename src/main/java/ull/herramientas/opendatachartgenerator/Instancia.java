14c4e366da5a1db73a52dcea390fda39fab37722
Joey <Joey@192.168.1.35>
2016-12-08 16:02:11 +0000

    Comentando ficheros Doxygen

--- src/main/java/ull/herramientas/opendatachartgenerator/Instancia.java
+++ src/main/java/ull/herramientas/opendatachartgenerator/Instancia.java
@@ -2,14 +2,20 @@ package ull.herramientas.opendatachartgenerator;
 
 import java.util.ArrayList;
 
+/**
+ * \class Instancia
+ * \brief Clase que almacenará las distintas instancias del dataset.
+ * \author Joel Perez Ramos
+ *
+ */
 public class Instancia {
 	/**
-	 * ArrayList de tipo Atributo
+	 * \brief ArrayList de tipo Atributo
 	 */
 	private ArrayList<Atributo> inst_;
-	
+
 	/**
-	 * Constructor que almacenará la instancia
+	 * \brief Constructor que almacenará la instancia
 	 */
 	public Instancia() {
 		super();
@@ -17,80 +23,90 @@ public class Instancia {
 	}
 	
 	/**
-	 * Metodo que retorna la instancia
-	 * @return
+	 * \brief Constructor que almacenará un conjunto de datos
+	 * \param atributos
+	 */
+	public Instancia(Atributo[] atributos){
+		this.inst_ = new ArrayList<Atributo>();
+		for (int i = 0; i<atributos.length; i++)
+			inst_.add(atributos[i]);
+	}
+
+	/**
+	 * \brief Metodo que retorna la instancia
+	 * \return
 	 */
 	public ArrayList<Atributo> getInst_() {
 		return inst_;
 	}
-	
+
 	/**
-	 * Metodo que modifica la instancia
-	 * @param inst_
+	 * \brief Metodo que modifica la instancia
+	 * \param inst_
 	 */
 	public void setInst_(ArrayList<Atributo> inst_) {
 		this.inst_ = inst_;
 	}
-	
+
 	/**
-	 * Metodo que añade un atributo a la instancia
-	 * @param A
+	 * \brief Metodo que añade un atributo a la instancia
+	 * \param A
 	 */
 	public void addAtributo(Atributo A) {
 		inst_.add(A);
 	}
-	
+
 	/**
-	 * Metodo que borra un atributo de la instancia
-	 * @param A
+	 * \brief Metodo que borra un atributo de la instancia
+	 * \param A
 	 */
 	public void removeAtributo(Atributo A) {
 		inst_.remove(A);
 	}
-	
+
 	/**
-	 * Metodo que retorna el valor de una posicion
+	 * \brief Metodo que retorna el valor de una posicion
 	 * de la instancia
-	 * @param pos
-	 * @return
+	 * \param pos
+	 * \return
 	 */
 	public String getValorItem(int pos) {
 		return inst_.get(pos).getValor();
 	}
-	
+
 	/**
-	 * Metodo que modifica el valor de una posicion
+	 * \brief Metodo que modifica el valor de una posicion
 	 * de la instancia
-	 * @param pos
-	 * @param val
+	 * \param pos
+	 * \param val
 	 */
 	public void setValorItem(int pos, String val) {
 		inst_.get(pos).setValor(val);
 	}
-	
+
 	/**
-	 * Metodo que retorna el id de una posicion
+	 * \brief Metodo que retorna el id de una posicion
 	 * de la instancia
-	 * @param pos
-	 * @return
+	 * \param pos
+	 * \return
 	 */
 	public String getIdItem(int pos) {
 		return inst_.get(pos).getId();
 	}
-	
+
 	/**
-	 * Metodo que modifica el id de una posicion
+	 * \brief Metodo que modifica el id de una posicion
 	 * de la instancia
-	 * @param pos
-	 * @param i
+	 * \param pos
+	 * \param i
 	 */
 	public void setIdItem(int pos, String i) {
 		inst_.get(pos).setId(i);
 	}
-	
+
 	/**
-	 * Metodo que retorna el tamaño
-	 * @return
+	 * \brief Metodo que retorna el tamaño
+	 * \return
 	 */
 	public int size() {
 		return inst_.size();
