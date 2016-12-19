009a3cfb3cdcee7720d878842fbe89f088eaefd3
Mauricio José Orta Rodríguez <sextus70@gmail.com>
2016-12-19 21:37:33 +0000

    Nueva versión de ventana principal

--- src/main/java/ull/herramientas/opendatachartgenerator/salida/VentanaPrincipal.java
+++ src/main/java/ull/herramientas/opendatachartgenerator/salida/VentanaPrincipal.java
@@ -9,10 +9,13 @@ import javax.swing.JTextField;
 
 import ull.herramientas.opendatachartgenerator.Dataset;
 
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.awt.event.ActionEvent;
+import javax.swing.JRadioButton;
+import javax.swing.JComboBox;
 
 /**
  * Ventana principal de la aplicación
@@ -22,7 +25,6 @@ import java.awt.event.ActionEvent;
 public class VentanaPrincipal {
 
 	private JFrame frame;
-	private JTextField urlConjunto;
 
 	/**
 	 * Lanza la aplicación.
@@ -56,24 +58,69 @@ public class VentanaPrincipal {
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		//Creación de radio botón de conjunto de datos de población de Barrios de Santa Cruz de Tenerife
+		JRadioButton rdbtnCsv = new JRadioButton(".csv");
+		rdbtnCsv.setBounds(127, 143, 67, 23);
+		frame.getContentPane().add(rdbtnCsv);
+
+		JRadioButton rdbtntxt = new JRadioButton(".txt");
+		rdbtntxt.setBounds(196, 143, 67, 23);
+		frame.getContentPane().add(rdbtntxt);
+
+		rdbtnCsv.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (rdbtntxt.isSelected())
+				{
+					rdbtntxt.setSelected(false);
+				}
+			}
+		});
+
+		rdbtntxt.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (rdbtnCsv.isSelected())
+				{
+					rdbtnCsv.setSelected(false);
+				}
+			}
+		});
         //Creación de título de aplicación
 		JLabel lblOpenDataChart = new JLabel("Open Data Chart Generator");
 		lblOpenDataChart.setFont(new Font("Tahoma", Font.BOLD, 23));
 		lblOpenDataChart.setBounds(67, 55, 357, 28);
 		frame.getContentPane().add(lblOpenDataChart);
-        //Creación de campo de texto para URLs de conjuntos de datos
-		urlConjunto = new JTextField();
-		urlConjunto.setBounds(116, 139, 219, 20);
-		frame.getContentPane().add(urlConjunto);
-		urlConjunto.setColumns(10);
-        //Creación de botón para procesar una url introducida
+
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(77, 94, 302, 20);
+		frame.getContentPane().add(comboBox);
+
+		DefaultComboBoxModel datasets = new DefaultComboBoxModel();
+		datasets.addElement("Población de barrios de Santa Cruz de Tenerife");
+		comboBox.setModel(datasets);
+
+
+        //Creación de botón para procesar el conjunto de datos seleccionado
 		JButton btnProcesarConjuntoDe = new JButton("Procesar conjunto de datos");
 		btnProcesarConjuntoDe.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				Dataset dataset;
 				try {
+					String urlDataset = "";
+					if (comboBox.getSelectedIndex() == 0)
+					{
+						if (rdbtnCsv.isSelected())
+						{
+							urlDataset = "http://www.santacruzdetenerife.es/opendata/dataset/8363b662-0bdc-47e1-b9f6-65b536714f29/resource/ee814891-ba52-4e7c-b9e6-017c1bc43b6b/download/barrios.csv";
+						}
+						else if (rdbtntxt.isSelected())
+						{
+							urlDataset = "";
+
+						}
+
+					}
 					//Se extrae y almacenan los datos del conjunto del url en un objeto de clase Dataset
-					dataset = new Dataset(urlConjunto.getText());
+					dataset = new Dataset(urlDataset);
 					//Se crea la ventana de selección de gráficos de la información del conjunto de datos
 					SalidaGraficosFrame seleccion_graficos = new SalidaGraficosFrame(dataset);
 				} catch (IOException e) {
@@ -85,9 +132,12 @@ public class VentanaPrincipal {
 		btnProcesarConjuntoDe.setBounds(132, 184, 193, 23);
 		frame.getContentPane().add(btnProcesarConjuntoDe);
 
-		//Creación de texto descriptivo de campo de introducción de URL de conjunto de datos
-		JLabel lblUrlDeConjunto = new JLabel("URL de conjunto de datos");
-		lblUrlDeConjunto.setBounds(156, 114, 149, 14);
-		frame.getContentPane().add(lblUrlDeConjunto);
+
+
+
+
+
+
+
 	}
 }
