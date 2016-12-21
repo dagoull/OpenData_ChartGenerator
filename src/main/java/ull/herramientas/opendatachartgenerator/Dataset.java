e7a3e2ba09a18e980e74556850692025d71327b7
Mauricio José Orta Rodríguez <sextus70@gmail.com>
2016-12-21 19:36:59 +0000

    Actualización de documentación de clase Dataset TG-3

--- src/main/java/ull/herramientas/opendatachartgenerator/Dataset.java
+++ src/main/java/ull/herramientas/opendatachartgenerator/Dataset.java
@@ -3,12 +3,23 @@ package ull.herramientas.opendatachartgenerator;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
-
+/**
+ * Clase que representa a un conjunto de datos
+ * @author Mauricio
+ *
+ */
 public class Dataset {
 	private IReader reader;
 	private ArrayList<Instancia> rows;
 	private String[] columnas;
 
+	/**
+	 * Constructor del dataset
+	 * @param url - URL del dataset a procesar
+	 * @param mode - Formato del dataset a procesar
+	 * @throws MalformedURLException
+	 * @throws IOException
+	 */
 	public Dataset(String url, int mode) throws MalformedURLException, IOException
 	{
 		if (mode == 0)
@@ -21,67 +32,89 @@ public class Dataset {
 	}
 
 	/**
-	 * @return the reader
+	 * @return el lector de conjuntos de datos
 	 */
 	public IReader getReader() {
 		return reader;
 	}
 
 	/**
-	 * @param reader the reader to set
+	 * @param reader - el lector de conjuntos de datos a asignar
 	 */
 	public void setReader(IReader reader) {
 		this.reader = reader;
 	}
 
 	/**
-	 * @return the rows
+	 * @return las filas del conjunto de datos
 	 */
 	public ArrayList<Instancia> getRows() {
 		return rows;
 	}
 
 	/**
-	 * @param rows the rows to set
+	 * @param rows - las filas del conjunto de datos a asignar
 	 */
 	public void setRows(ArrayList<Instancia> rows) {
 		this.rows = rows;
 	}
 
 	/**
-	 * @return the columnas
+	 * @return los nombres de las columnas del conjunto de datos
 	 */
 	public String[] getColumnas() {
 		return columnas;
 	}
 
 	/**
-	 * @param columnas the columnas to set
+	 * @param columnas - los nombres de las columnas del conjunto de datos a asignar
 	 */
 	public void setColumnas(String[] columnas) {
 		this.columnas = columnas;
 	}
 
+	/**
+	 * @return el número de filas del conjunto de datos
+	 */
 	public int size()
 	{
 		return rows.size();
 	}
 
+	/**
+	 * Retorna una instancia del conjunto de datos guardada en el índice i de su array de filas.
+	 * @param i - Índice de array de filas.
+	 * @return
+	 */
 	public ArrayList<Atributo> getInstancia(int i)
 	{
 		return rows.get(i).getInst_();
 	}
 
+	/**
+	 * Retorna el número de atributos del conjunto de datos
+	 * @return
+	 */
 	public int numero_de_atributos()
 	{
 		return columnas.length;
 	}
 
+	/**
+	 * Retorna el nombre del atributo de la columna i del conjunto de datos
+	 * @param i - índice de columna de conjunto de datos
+	 * @return nombre del atributo de columna i de conjunto de datos
+	 */
 	public String getNombreAtributo(int i)
 	{
 		return columnas[i];
 	}
-	
+
+	/**
+	 * Devuelve la lista de valores pertenecientes a una columna del conjunto de datos
+	 * @param index - índice de columna del conjunto de datos
+	 * @return lista de valores de columna index del conjunto de datos
+	 */
 	public ArrayList<String> getColumna(int index){
 		ArrayList<String> aux=new ArrayList<String>();
 		for(int i=0; i<rows.size(); i++){
